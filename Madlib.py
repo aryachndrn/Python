51daace9066e5137947cb9a5268a71d2db97c733
arya31chndrn <aryaedakkara05@gmail.com>
2025-06-19 01:18:22 +0530

    1st

---
+++ Madlib.py
@@ -0,0 +1,27 @@
+print("Give appropriate words for each:")
+movie_name=input("movie name: ")
+name=input("name a friend: ")
+verb=input("verb: ")
+body=input("A body part:")
+name2=input("Another friend's name: ")
+he=input("Is the friend he/she: ")
+adj1=input("Adjective: ")
+adj2=input("Adjective: ")
+verb1=input("verb: ")
+weapon=input("weapon: ")
+body2=input("A body part: ")
+verb2=input("verb: ")
+adverb=input("adverb: ")
+food=input("Food: ")
+beg=input("beginning/end: ")
+adj3=input("Adjective: ")
+
+
+
+madlib=f"Once upon a time in the Kingdom of {movie_name}, dragons and {name} had a complicated relationship — mostly \
+because the dragons were {verb} at keeping secrets, and the {name} was even worse at keeping their {body} clean. Sir \
+{name2}, the kingdom’s most {adj1} and {adj2} knight, once tried to {verb1} a dragon by juggling flaming \
+{weapon}. Unfortunately, {name2} was so nervous that {he} dropped one mid-air, which landed on his own {body2}. The dragon, instead\
+of roasting him, burst into uncontrollable {verb2}, sending out a plume of smoke that smelled {adverb} like burnt \
+{food}. It was the {beg} of a rather {adj3} friendship."
+print(madlib)
